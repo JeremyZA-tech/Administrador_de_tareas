f14a47e1af23ebadcd6bc92ccb9adfce2b4071c6
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2023-10-14 20:20:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/administrador_de_tareas/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.administrador_de_tareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.administrador_de_tareas.ui.theme.Administrador_de_tareasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Administrador_de_tareasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Administrador_de_tareasTheme {
+        Greeting("Android")
+    }
+}
