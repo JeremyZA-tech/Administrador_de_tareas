74be00f91df0262ec114dac66f1f8220d17dc922
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2023-10-14 20:20:38 +0200

    Initial commit

--- app/src/main/java/com/example/administrador_de_tareas/MainActivity.kt
+++ app/src/main/java/com/example/administrador_de_tareas/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.administrador_de_tareas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.administrador_de_tareas.ui.theme.Administrador_de_tareasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Tasks(stringResource(R.string.all_tasks_completed), stringResource(R.string.nice_work))
                 }
             }
         }
@@ -30,17 +40,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Tasks(result: String, messege: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ){
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = result,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = messege,
+            fontSize = 16.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Administrador_de_tareasTheme {
-        Greeting("Android")
+        Tasks(stringResource(R.string.all_tasks_completed), stringResource(R.string.nice_work))
     }
 }
